ffaff111556b0d9d288ec25eb861c6b31c4f4b2e
Axel Charrier <axel.charrier56@ecoles-epsi.net>
2026-01-16 10:56:45 +0100

    Création du model Student

--- StudentApp.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace StudentApp.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ StudentApp.Domain/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentApp.Domain.Models
+{
+    public sealed record class Student
+    {
+        public required int Id { get; set; }
+        public required string LastName { get; set; }
+        public required string FirstName { get; set; }
+    }
+}
