ecef7f3de665b0aa8a2e5903a145411f45e99098
Axel Charrier <axel.charrier56@ecoles-epsi.net>
2026-01-19 15:20:08 +0100

    Mise en place du DTO et du service GetAll

---
+++ StudentApp.Application/Implementations/StudentService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.Pkcs;
+using System.Text;
+using StudentApp.Application.Models;
+using StudentApp.Domain.Models;
+using StudentApp.Infrastructure.Repositories;
+
+namespace StudentApp.Application.Implementations
+{
+    public interface IStudentService
+    {
+        StudentDto[] GetAllStudents();
+        StudentDto GetStudent(int id);
+        int AddStudent(StudentDto student);
+        StudentDto UpdateStudent(StudentDto student);
+        Boolean DeleteStudent(int id);
+    }
+    internal class StudentService
+    {
+        private readonly IStudentRepository StudentRepository;
+        
+        public StudentService(IStudentRepository _studentRepository)
+        {
+            StudentRepository = _studentRepository;
+        }
+
+        public StudentDto[] GetAllStudents()
+        {
+            var students = StudentRepository.getAllStudents();
+            StudentDto[] studentsDto = [];
+
+            foreach (Student student in students)
+            {
+                var studentToAdd = new StudentDto(student.Id, student.FirstName,
+                    student.LastName);
+                studentsDto.Append(studentToAdd);
+            }
+
+            return studentsDto;
+        }
+    }
+}
---
+++ StudentApp.Application/Models/StudentDto.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentApp.Application.Models
+{
+    public record StudentDto(int Id, String FirstName, String LastName) { }
+}
