af69c36eab1685ae13a1ec995788370a520a14a4
Axel Charrier <axel.charrier56@ecoles-epsi.net>
2026-01-19 14:34:27 +0100

    Création de repo Student

---
+++ StudentApp.Infrastructure/Repositories/StudentRepository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentApp.Domain.Models;
+using StudentApp.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentApp.Infrastructure.Repositories
+{
+    public interface IStudentRepository
+    {
+        IEnumerable<Student> getAllStudents();
+        Student? getStudentById(int id);
+        int AddStudent(Student studentToAdd);
+        Student UpdateStudent(Student studentToUpdate);
+        Boolean DeleteStudent(int id);
+
+    }
+
+    internal class StudentRepository
+    {
+        private readonly MyDbContext Context;
+
+        public StudentRepository(MyDbContext _context)
+        {
+            Context = _context;
+        }
+
+        public IEnumerable<Student> getAllStudents()
+        {
+            var list = Context.Students.ToList();
+            return list;
+        }
+
+        public Student? getStudentById(int id) {
+            var studentToReturn = Context.Students.Find(id);
+            return studentToReturn;
+        }
+
+        public int AddStudent(Student studentToAdd)
+        {
+            Context.Students.Add(studentToAdd);
+            Context.SaveChanges();
+            return studentToAdd.Id;
+        }
+
+        public Student UpdateStudent(Student studentToUpdate)
+        {
+            Context.Update(studentToUpdate);
+            Context.SaveChanges();
+            return studentToUpdate;
+        }
+
+        public Boolean DeleteStudent(int id)
+        {
+            var studentToDelete = Context.Students.Find(id);
+            if (studentToDelete != null)
+            {
+                Context.Students.Remove(studentToDelete);
+                Context.SaveChanges();
+            }
+            return true;
+        }
+    }
+}
