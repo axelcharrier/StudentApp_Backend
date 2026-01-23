9d09141c759ec773eb511797c1f4b436bf9d4262
Axel Charrier <axel.charrier56@ecoles-epsi.net>
2026-01-23 15:56:04 +0100

    Initialisation de la minimale Api

---
+++ StudentApp.ApiMinimal/Program.cs
@@ -0,0 +1,44 @@
+using Scalar.AspNetCore;
+using StudentApp.Application.Abstraction;
+using StudentApp.Application.Extensions;
+using StudentApp.Application.Implementations;
+using StudentApp.Infrastructure.Abstractions;
+using StudentApp.Infrastructure.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationServices(builder.Configuration);
+
+// Add services to the container.
+builder.Services.AddScoped<IStudentService, StudentService>();
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+
+// Cors policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAngularOrigins",
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                      });
+});
+
+builder.Services.AddOpenApi();
+
+
+var app = builder.Build();
+
+#region Méthodes
+
+#endregion 
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseCors("AllowAngularOrigins");
+
+await app.RunAsync();
