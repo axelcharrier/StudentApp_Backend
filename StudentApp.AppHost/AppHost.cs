09217bbfc507fc6c7f0b472e7bf0a9b779c82c95
Axel Charrier <axel.charrier56@ecoles-epsi.net>
2026-01-30 10:56:54 +0100

    Ajout de la base de données dans Aspire

--- StudentApp.AppHost/AppHost.cs
+++ StudentApp.AppHost/AppHost.cs
@@ -2,6 +2,16 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<StudentApp_ApiMinimal>("Api");
+var sqlServer = builder.AddSqlServer("sql", port: 1433)
+    .WithBindMount("c:\\temp\\docker\\mssql\\log", "/var/opt/mssql/log")
+    .WithBindMount("c:\\temp\\docker\\mssql\\data", "/var/opt/mssql/data")
+    .WithBindMount("c:\\temp\\docker\\mssql\\secrets", "/var/opt/mssql/secrets")
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var sqlDatabase = sqlServer.AddDatabase("students");
+
+var api = builder.AddProject<StudentApp_ApiMinimal>("Api")
+    .WithReference(sqlDatabase)
+    .WaitFor(sqlDatabase);
 
 builder.Build().Run();
