cd18bbcf80412961a8a2444936b1b31307f84152
Axel Charrier <axel.charrier56@ecoles-epsi.net>
2026-02-16 15:10:39 +0100

    correction PR

--- StudentApp.Domain/Entities/User.cs
+++ StudentApp.Domain/Entities/User.cs
@@ -2,7 +2,7 @@
 
 public sealed record class User
 {
-    public string Mail { get; set; }
+    public required string Mail { get; set; }
     public bool IsMailConfirmed { get; set; }
-    public string Role { get; set; }
+    public required string Role { get; set; }
 }
