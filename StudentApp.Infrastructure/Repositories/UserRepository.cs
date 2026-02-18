ae4fe934f216607713eaf706c84e8fa8cc53fff4
Axel Charrier <axel.charrier56@ecoles-epsi.net>
2026-02-18 16:07:06 +0100

    Fix après commentaires

--- StudentApp.Infrastructure/Repositories/UserRepository.cs
+++ StudentApp.Infrastructure/Repositories/UserRepository.cs
@@ -76,8 +76,6 @@ public class UserRepository(AppDbContext context) : IUserRepository
     }
 
     /// <inheritdoc/>
-    public async Task<bool> RoleExistsAsync(string roleName, CancellationToken ct)
-    {
-        return await context.Roles.FirstOrDefaultAsync(role => role.Name == roleName, cancellationToken: ct) != null;
-    }
+    public async Task<bool> RoleExistsAsync(string roleName, CancellationToken ct) =>
+        await context.Roles.AnyAsync(role => role.Name != null && role.Name.Equals(roleName, StringComparison.OrdinalIgnoreCase), cancellationToken: ct).ConfigureAwait(false);
 }
