39adc6ec4235f484e3286bddc26bea581f8b91c3
Axel Charrier <axel.charrier56@ecoles-epsi.net>
2026-02-23 15:39:26 +0100

    Création des mappers

---
+++ StudentApp.Application/Mappers/StudentMapper.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentApp.Application.Mappers;
+
+using Riok.Mapperly.Abstractions;
+using StudentApp.Application.Models.Dto;
+using StudentApp.Domain.Entities;
+
+[Mapper]
+public static partial class StudentMapper
+{
+    [MapProperty(nameof(Student.FirstName), nameof(StudentDto.FirstName))]
+    [MapProperty(nameof(Student.LastName), nameof(StudentDto.LastName))]
+    [MapProperty(nameof(Student.Id), nameof(StudentDto.Id))]
+
+    public static partial StudentDto ToStudentDto(this Student student);
+}
---
+++ StudentApp.Application/Mappers/UserMapper.cs
@@ -0,0 +1,30 @@
+﻿namespace StudentApp.Application.Mappers;
+
+using Riok.Mapperly.Abstractions;
+using StudentApp.Application.Models.Dto;
+using StudentApp.Domain.Entities;
+
+[Mapper]
+public static partial class UserMapper
+{
+    [MapProperty(nameof(User.UserName), nameof(UserDto.Mail))]
+    [MapProperty(nameof(User.Role), nameof(UserDto.Role))]
+    [MapProperty(nameof(User.EmailConfirmed), nameof(UserDto.IsMailConfirmed))]
+
+    [MapperIgnoreSource(nameof(User.Id))]
+    [MapperIgnoreSource(nameof(User.NormalizedUserName))]
+    [MapperIgnoreSource(nameof(User.NormalizedEmail))]
+    [MapperIgnoreSource(nameof(User.PasswordHash))]
+    [MapperIgnoreSource(nameof(User.SecurityStamp))]
+    [MapperIgnoreSource(nameof(User.ConcurrencyStamp))]
+    [MapperIgnoreSource(nameof(User.PhoneNumber))]
+    [MapperIgnoreSource(nameof(User.PhoneNumberConfirmed))]
+    [MapperIgnoreSource(nameof(User.LockoutEnd))]
+    [MapperIgnoreSource(nameof(User.LockoutEnabled))]
+    [MapperIgnoreSource(nameof(User.AccessFailedCount))]
+    [MapperIgnoreSource(nameof(User.Email))]
+    [MapperIgnoreSource(nameof(User.IsMailConfirmed))]
+    [MapperIgnoreSource(nameof(User.Mail))]
+    [MapperIgnoreSource(nameof(User.TwoFactorEnabled))]
+    public static partial UserDto ToUserDto(this User user);
+}
